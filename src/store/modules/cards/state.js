5db6f69f4403cd26cc6d4399a0887936674acc9f
Felipe Schons <fwiebkeschons@gmail.com>
2024-06-14 11:07:46 -0300

    adicionado propriedade de prioridade para os cards

--- src/store/modules/cards/state.js
+++ src/store/modules/cards/state.js
@@ -4,37 +4,43 @@ export default {
       id: 1,
       title: 'titulo 1',
       desc: 'lorem ipsum',
-      status: 2
+      status: 2,
+      priority: 0
     },
     {
       id: 2,
       title: 'titulo 2',
       desc: 'lorem ipsum',
-      status: 1
+      status: 1,
+      priority: 0
     },
     {
       id: 3,
       title: 'titulo 3',
       desc: 'lorem ipsum',
-      status: 1
+      status: 1,
+      priority: 0
     },
     {
       id: 4,
       title: 'titulo 4',
       desc: 'lorem ipsum',
-      status: 1
+      status: 1,
+      priority: 0
     },
     {
       id: 5,
       title: 'titulo 5',
       desc: 'lorem ipsum',
-      status: 0
+      status: 0,
+      priority: 0
     },
     {
       id: 6,
       title: 'titulo 6',
       desc: 'lorem ipsum',
-      status: 0
+      status: 0,
+      priority: 0
     }, 
   ]
 }
